c65ce6fca7d3fc92f714ce720a56cba0a5a21581
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-03-05 14:09:04 +0000

    added temp change and bgcolor

--- js/weatherApp.js
+++ js/weatherApp.js
@@ -1,32 +1,31 @@
-$.getJSON('http://weather-api.herokuapp.com/cities', function(data){
+$(document).ready(function(){
 
-  var apiData = data.cities;
 
-  var weatherIcon = 'snow';
-  $.each(apiData, function(index, city) {
-  $('.cities').append(
-    $('<option></option>').val(city).html(city + " ⌄")
-    );
+
+  $.getJSON('http://weather-api.herokuapp.com/cities', function(data){
+    var apiData = data.cities;
+    $.each(apiData, function(index, city) {
+    $('.cities').append(
+      $('<option></option>').val(city).html(city + " ⌄")
+      );
+    });  
   });
 
-  $('img').attr('src', 'images/' + weatherIcon + '.svg')
+  $('.cities').click(function() {
+    var city = $(this).val();
+    callJsonObj(city);
+  });
+
+  function callJsonObj(city){
+    $.getJSON('http://weather-api.herokuapp.com/temperature?city=' + city, function(data){
+      console.log(data);
+      var temperature = data.temp;
+      $('.temperature').text(temperature);
+      $('img').attr('src', 'images/' + data.outlook + '.svg');
+      $('body').addClass(data.outlook);
+    });  
+  };
 });
-
-$('.cities').click(function() {
-  var city = $(this).val();
-  callJsonObj(city);
-});
-
-
-function callJsonObj(city){
-  $.getJSON('http://weather-api.herokuapp.com/temperature?city=' + city, function(data){
-    console.log(data);
-    var temperature = data.temp;
-    $('.temperature').text(temperature);
-  });  
-};
-
-
 
 
 
